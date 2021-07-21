c74cc0351d3aadd567bed9604467a7642cff2efc
Carrie Mathieu <carrieburks@gmail.com>
2021-07-21 13:11:00 -0400

    length event listener

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  length = 0;
   includeLetters = false;
   includeNumbers = false;
   includeSymbols = false;
   password = '';
+
+  // typescript needs type for variable
+  onChangeLength(value: string) {
+    // converts string input (length) to int
+    const parsedValue = parseInt(value)
+
+    // checks if parsedValue is a number
+    if (!isNaN(parsedValue)) {
+      this.length = parsedValue
+    }
+  }
 
   onChangeUseLetters() {
     // this.includeLetters is equal to the opposite of whatever this.includeLetters currently is
